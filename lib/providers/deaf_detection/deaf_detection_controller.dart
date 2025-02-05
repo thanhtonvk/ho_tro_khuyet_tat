c5882b004a300da40f33feeb68f81f083fc1444b
tondt <tondt@dyno.vn>
2025-02-05 18:25:26 +0700

    update

---
+++ lib/providers/deaf_detection/deaf_detection_controller.dart
@@ -0,0 +1,61 @@
+import 'dart:async';
+import 'dart:developer';
+import 'dart:ui' as ui;
+
+import 'package:camera/camera.dart';
+import 'package:dart_ncnn_yolov8/dart_ncnn_flutter.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../my_camera_controller.dart';
+
+final faceDetectController =
+StateNotifierProvider<DeafDetectionController, List<YoloResult>>(
+  DeafDetectionController.new,
+);
+
+class DeafDetectionController extends StateNotifier<List<YoloResult>> {
+  DeafDetectionController(this.ref) : super([]);
+
+  final Ref ref;
+
+  final nguoiKhuyetTatSDK = NguoiKhuyetTatSdk();
+
+  static final previewImage = StateProvider<ui.Image?>(
+        (_) => null,
+  );
+
+  Future<void> initialize() async {
+    await nguoiKhuyetTatSDK.load(isBlind: false, isDeaf: true);
+  }
+  Future<void> detectDeaf(CameraImage cameraImage) async {
+    final completer = Completer<void>();
+    switch (cameraImage.format.group) {
+      case ImageFormatGroup.unknown:
+      case ImageFormatGroup.jpeg:
+        log('not support format');
+        return;
+      case ImageFormatGroup.yuv420:
+        state = nguoiKhuyetTatSDK
+            .detectDeafYUV420(
+          y: cameraImage.planes[0].bytes,
+          u: cameraImage.planes[1].bytes,
+          v: cameraImage.planes[2].bytes,
+          height: cameraImage.height,
+          deviceOrientationType:
+          ref.read(myCameraController).deviceOrientationType,
+          sensorOrientation: ref.read(myCameraController).sensorOrientation,
+          onDecodeImage: (image) {
+            ref.read(previewImage.notifier).state = image;
+            completer.complete();
+          },
+        )
+            .result;
+        break;
+      case ImageFormatGroup.nv21:
+        break;
+      case ImageFormatGroup.bgra8888:
+        break;
+    }
+    return completer.future;
+  }
+}
---
+++ lib/providers/object_detection/object_detection_controller.dart
@@ -0,0 +1,93 @@
+import 'dart:async';
+import 'dart:developer';
+import 'dart:ui' as ui;
+
+import 'package:camera/camera.dart';
+import 'package:dart_ncnn_yolov8/dart_ncnn_flutter.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../my_camera_controller.dart';
+
+final faceDetectController =
+    StateNotifierProvider<ObjectDetectionController, List<YoloResult>>(
+  ObjectDetectionController.new,
+);
+
+class ObjectDetectionController extends StateNotifier<List<YoloResult>> {
+  ObjectDetectionController(this.ref) : super([]);
+
+  final Ref ref;
+
+  final nguoiKhuyetTatSDK = NguoiKhuyetTatSdk();
+
+  static final previewImage = StateProvider<ui.Image?>(
+    (_) => null,
+  );
+
+  Future<void> initialize() async {
+    await nguoiKhuyetTatSDK.load(isBlind: true, isDeaf: false);
+  }
+  Future<void> detectMoney(CameraImage cameraImage) async {
+    final completer = Completer<void>();
+    switch (cameraImage.format.group) {
+      case ImageFormatGroup.unknown:
+      case ImageFormatGroup.jpeg:
+        log('not support format');
+        return;
+      case ImageFormatGroup.yuv420:
+        state = nguoiKhuyetTatSDK
+            .detectMoneyYUV420(
+          y: cameraImage.planes[0].bytes,
+          u: cameraImage.planes[1].bytes,
+          v: cameraImage.planes[2].bytes,
+          height: cameraImage.height,
+          deviceOrientationType:
+          ref.read(myCameraController).deviceOrientationType,
+          sensorOrientation: ref.read(myCameraController).sensorOrientation,
+          onDecodeImage: (image) {
+            ref.read(previewImage.notifier).state = image;
+            completer.complete();
+          },
+        )
+            .result;
+        break;
+      case ImageFormatGroup.nv21:
+        break;
+      case ImageFormatGroup.bgra8888:
+        break;
+    }
+    return completer.future;
+  }
+
+  Future<void> detectObject(CameraImage cameraImage) async {
+    final completer = Completer<void>();
+    switch (cameraImage.format.group) {
+      case ImageFormatGroup.unknown:
+      case ImageFormatGroup.jpeg:
+        log('not support format');
+        return;
+      case ImageFormatGroup.yuv420:
+        state = nguoiKhuyetTatSDK
+            .detectObjectYUV420(
+              y: cameraImage.planes[0].bytes,
+              u: cameraImage.planes[1].bytes,
+              v: cameraImage.planes[2].bytes,
+              height: cameraImage.height,
+              deviceOrientationType:
+                  ref.read(myCameraController).deviceOrientationType,
+              sensorOrientation: ref.read(myCameraController).sensorOrientation,
+              onDecodeImage: (image) {
+                ref.read(previewImage.notifier).state = image;
+                completer.complete();
+              },
+            )
+            .result;
+        break;
+      case ImageFormatGroup.nv21:
+        break;
+      case ImageFormatGroup.bgra8888:
+        break;
+    }
+    return completer.future;
+  }
+}
